af3143eaf4a234ec21e22dfc36deae989777a3e4
manthanparekh <manthan.parekh@outlook.com>
2023-06-25 23:24:00 -0400

    addtodo.js

---
+++ src/components/AddTodo.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const AddTodo ({ addTodo }) => {
+    const [content, setContent] = useState('');
+    const toast = useToast();
+}
+
+export default AddTodo;
