e3b99c0e85af552bbb095ac3a38b245c937e8ab3
manthanparekh <manthan.parekh@outlook.com>
2023-06-29 13:03:54 -0400

    most-todolist-js-2

--- .history/src/components/TodoList_20230629125903.js
+++ .history/src/components/TodoList_20230629125903.js
@@ -4,12 +4,12 @@ import React from 'react';
 const TodoList = ({ todos, deleteItem }) => {
     if (!todos.length) {
         return (
-            <Badge colorScheme='cyan' p='4' borderRadius='1g'>No task Todo</Badge>
+            <Ba
         );
     }
 
     return (
-        <VStack divider={<StackDivider />} borderWidth='2px' bordorColor='gray.100' bordorRadius='lg' padding='4' w='100%' maxW={{base: '90vw', sm: '80vw', lg: '50vw', x1: '40vw'}} alignItems='stretch'>
+        <VStack divider={<StackDivider />} borderWidth='2px' bordorColor='gray.100' bordorRadius='lg' padding='4' w='100%' maxW={{base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw'}} alignItems='stretch'>
             {todos.map(todo => (
                 <HStack key={todo.id}>
                     <Text>{todo.body}</Text>
