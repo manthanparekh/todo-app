42eaa9fca7a4aacce5e7132047c9eea6d7a6521d
manthanparekh <manthan.parekh@outlook.com>
2023-06-29 11:55:12 -0400

    todolist-js-1

---
+++ .history/src/App_20230624235708.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const deleteTask(id) {
+    const newTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/TodoList_20230627212614.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+
+const TodoList = ({ todos, deleteItem }) => {
+    if (!todos.length) {
+        return (
+            <Ba
+        );
+    }
+
+    return (
+        <VStack divider={}
+    )
+}
